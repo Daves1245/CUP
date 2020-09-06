c9a292b4b4d7e7d131f3795b72e6b303494e1b5e
David Santamaria <davidsantamaria1245@gmail.com>
2020-09-06 17:33:44 -0400

    fixed heap

--- datastructures/heap/heap.c
+++ datastructures/heap/heap.c
@@ -1,125 +1,160 @@
 #include <string.h>
 #include <stdio.h>
+#include <time.h>
 #include <stdlib.h>
 
 #include "heap.h"
 
 #define HEAP_SIZE 100
 #define HEAP_MAX_ELEM_VALUE 1000
+#define ROOT 1
 
-#define HEAP_SWAP(type, arr, i, j) \
-    do { \
-        type tmp = arr[i]; \
-        arr[i] = arr[j]; \
-        arr[j] = tmp; \
-    } while (0);
+void swap(int *arr, int a, int b) {
+    int tmp = arr[a];
+    arr[a] = arr[b];
+    arr[b] = tmp;
+}
 
-// TODO check discord
-
-static inline int randval(int min, int max) {
+int randval(int min, int max) {
     return min + rand() / (RAND_MAX / (max - min + 1) + 1);
 }
 
 void fill_with_random(int *arr, struct heap_context *actx) {
-    for (int i = 0; i < actx->arr_len; i++) {
+    arr[0] = -1;
+    for (int i = ROOT; i <= actx->arr_len; i++) {
         arr[i] = randval(actx->min_elem_val, actx->max_elem_val);
     }
 }
 
-static inline int greater(int a, int b) {
+int greater(int a, int b) {
     return a > b;
 }
 
-static inline int lesser(int a, int b) {
+int lesser(int a, int b) {
     return a < b;
 }
 
+void *assurealloc(size_t len) {
+    void *p = malloc(len);
+    if (!p) {
+        fprintf(stderr, "[assurealloc]: failed to allocate memory.\nAbort.\n");
+        exit(EXIT_FAILURE);
+    }
+}
 
 /* Driver - test heapsort algorithm */
 int main(int argc, char **argv) {
     int *arr;
-    struct heap_context actx = {
+    struct heap_context ctx = {
+        HEAP_SIZE,
         HEAP_SIZE,
         HEAP_MAX_ELEM_VALUE,
         0,
-        HEAP_SIZE,
         greater
     };
+    srand(time(NULL));
 
     if (argc > 1) {
-        actx.arr_len = atoi(argv[1]);
+        ctx.arr_len = atoi(argv[1]);
     }
     if (argc > 2) {
-        actx.max_elem_val = atoi(argv[2]);
+        ctx.max_elem_val = atoi(argv[2]);
     }
     if (argc > 3) {
-        actx.min_elem_val = atoi(argv[3]);
+        ctx.min_elem_val = atoi(argv[3]);
     }
 
-    arr = malloc(sizeof(int) * actx.heap_size);
-    memset(arr, 0, sizeof(int) * actx.heap_size);
-    fill_with_random(arr, &actx);
-    heapsort(arr, &actx);
-
-    for (int i = 0; i < actx.arr_len; i++) {
+    arr = assurealloc(sizeof(int) * (ctx.heap_size + 1));
+    fill_with_random(arr, &ctx);
+    heapsort(arr, &ctx);
+    for (int i = ROOT; i <= ctx.arr_len; i++) {
         printf("%d ", arr[i]);
     }
-    puts("");
-    return 0;
+    puts("Checking if array is sorted... ");
+    for (int i = ROOT; i < ctx.arr_len; i++) {
+        if (arr[i] > arr[i] + 1) {
+            puts("FAILED!");
+            exit(EXIT_FAILURE);
+        }
+    }
+    puts("PASSED!");
+    exit(EXIT_SUCCESS);
 }
 
-static inline int parent(int i) {
-    return (i - 2) / 2;
+int parent(int i) {
+    return i / 2;
 }
 
 int left(int i) {
-    return 2 * i + 1;
+    return 2 * i;
 }
 
 int right(int i) {
-    return 2 * i + 2;
+    return 2 * i + 1;
 }
 
 /* Heapify an array */
 void heapify(int *arr, struct heap_context *hctx, int i) {
     int swapi = i;
 
-    if (left(i) < hctx->heap_size && hctx->comp(arr[left(i)], arr[i])) {
+    if (left(i) <= hctx->heap_size && hctx->comp(arr[left(i)], arr[i])) {
         swapi = left(i);
     }
 
-    if (right(i) < hctx->heap_size && hctx->comp(arr[right(i)], arr[swapi])) {
+    if (right(i) <= hctx->heap_size && hctx->comp(arr[right(i)], arr[swapi])) {
         swapi = right(i);
     }
 
     if (i != swapi) {
-        HEAP_SWAP(int, arr, i, swapi);
+        swap(arr, i, swapi);
         heapify(arr, hctx, swapi);
+    }
+}
+
+void percolateDown(int *arr, struct heap_context *hctx, int i) {
+    int swapi = i;
+    if (left(i) <= hctx->heap_size && hctx->comp(arr[left(i)], arr[i])) {
+        swapi = left(i);
+    }
+    if (right(i) <= hctx->heap_size && hctx->comp(arr[right(i)], arr[swapi])) {
+        swapi = right(i);
+    }
+
+    if (i != swapi) {
+        swap(arr, i, swapi);
+        percolateDown(arr, hctx, swapi);
+    }
+}
+
+int *insert(int *arr, struct heap_context *hctx, int i) {
+    if (hctx->heap_size == hctx->arr_len) {
+        return NULL;
+    }
+
+    
+}
+
+void percolateUp(int *arr, struct heap_context *hctx, int i) {
+    while (i != ROOT && hctx->comp(arr[parent(i)], arr[i])) {
+        swap(arr, i, parent(i));
+        i = parent(i);
     }
 }
 
 /* Build a heap from an unsorted array */
 void build_heap(int *arr, struct heap_context *hctx) {
     hctx->heap_size = hctx->arr_len;
-    for (int i = hctx->heap_size; i > 0; i = parent(i)) {
-        heapify(arr, hctx, i);
+    for (int i = hctx->heap_size; i >= ROOT; i--) {
+        percolateDown(arr, hctx, i);
     }
 }
 
 /* Sort using heap */
 void heapsort(int *arr, struct heap_context *hctx) {
     build_heap(arr, hctx);
-    for (int i = hctx->arr_len - 1; i > 0; i--) {
-        HEAP_SWAP(int, arr, 0, i);
+    for (int i = hctx->arr_len; i >= ROOT; i--) {
+        swap(arr, i, ROOT);
         hctx->heap_size--;
-        heapify(arr, hctx, 0);
+        percolateDown(arr, hctx, ROOT);
     }
 }
-
-void printarr(int *arr, size_t len) {
-    for(int i = 0; i < len; i++) {
-        printf("%d ", arr[i]);
-    }
-    printf("\n");
-}
-
--- datastructures/heap/heap.h
+++ datastructures/heap/heap.h
@@ -1,25 +1,11 @@
 #ifndef CUP_HEAP_H
 #define CUP_HEAP_H
 
-#define HEAP_SWAP(type, arr, i, j) \
-    do { \
-        type tmp = arr[i]; \
-        arr[i] = arr[j]; \
-        arr[j] = tmp; \
-    } while (0);
-
-// XXX
-
-struct node {
-    struct node *left, *right, *parent;
-    int data;
-};
-
 struct heap_context {
     int arr_len;
+    int heap_size;
     int max_elem_val;
     int min_elem_val;
-    int heap_size;
     int (*comp)(int, int); /* comparator function */
 };
 
@@ -30,6 +16,10 @@ int heap_pop(int *arr, size_t len, int (*comp)(int, int));
 void heapify(int *arr, struct heap_context *hctx, int i);
 /* Build a heap with comparator func */
 void build_heap(int *arr, struct heap_context *hctx);
+/* Perculate an item down the heap */
+void percolateDown(int *arr, struct heap_context *hctx, int i);
+/* Perculate an item up the heap */
+void percolateUp(int *arr, struct heap_context *hctx, int i);
 
 /* Sort array using a heap */
 void heapsort(int *arr, struct heap_context *hctx);
